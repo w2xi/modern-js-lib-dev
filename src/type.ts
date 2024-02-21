8a08eca4ae938517628aea4b88e064aa259b8d7e
w2xi <43wangxi@gmail.com>
2024-02-21 22:40:48 +0800

    测试修改代码 #1

--- src/type.ts
+++ src/type.ts
@@ -2,6 +2,10 @@ export function isObject(value: any) {
   return value != null && typeof value === 'object'
 }
 
+export function isNumber(value: any) {
+  return typeof value === 'number'
+}
+
 export function type(value: any) {
   return Object.prototype.toString.call(value).slice(8, -1).toLowerCase()
 }
